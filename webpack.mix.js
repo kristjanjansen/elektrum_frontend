80dd62935256e242aab499d07a34bcc71964307e
kristjanjansen <kristjan.jansen@gmail.com>
2017-03-26 11:04:31 +0300

    init

---
+++ webpack.mix.js
@@ -0,0 +1,57 @@
+var mix = require('laravel-mix');
+
+// Theme
+
+mix.less('src/theme/theme.less', 'dist/theme.css');
+
+// Custom
+
+mix.styles([
+    'src/custom/cuthere.css',
+    'src/custom/reiven.css'
+], 'dist/custom.css');
+
+// Vendor
+
+mix.styles([
+    'node_modules/jquery-ui-dist/jquery-ui.css',
+    'node_modules/select2/dist/css/select2.css',
+    'node_modules/datatables/media/css/jquery.dataTables.css',
+    'node_modules/daterangepicker/daterangepicker-bs3.css',
+    'node_modules/jquery-ui-dist/jquery-ui.css',
+    'node_modules/jquery-ui-timepicker-addon/dist/jquery-ui-timepicker-addon.css',
+    'node_modules/fancybox/dist/css/jquery.fancybox.css',
+    'node_modules/bootstrap-tokenfield/dist/css/bootstrap-tokenfield.css',
+    'node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css',
+], 'dist/vendor.css');
+
+mix.scripts([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/select2/dist/js/select2.full.js',
+    'node_modules/datatables/media/js/jquery.dataTables.js',
+    'node_modules/moment/moment.js',
+    'node_modules/daterangepicker/daterangepicker.js',
+    'node_modules/jquery-ui-dist/jquery-ui.js',
+    'node_modules/jquery-ui-timepicker-addon/dist/jquery-ui-timepicker-addon.js',
+    'node_modules/fancybox/dist/js/jquery.fancybox.js',
+    'node_modules/bootstrap-tokenfield/dist/bootstrap-tokenfield.js',
+    'node_modules/jquery-form/src/jquery.form.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js',
+    'node_modules/underscore/underscore.js',
+    'node_modules/backbone/backbone.js',
+    'node_modules/bootbox/bootbox.js',
+    'node_modules/jquery-masked-input/dist/jquery.masked-input.js',
+    'node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js',
+], 'dist/vendor.js');
+
+/*
+
+Possible additions:
+
+    yarn add html5shiv excanvas jquery-placeholder
+
+    'node_modules/html5shiv/dist/html5shiv.js',
+    'node_modules/excanvas/excanvas.js',
+    'node_modules/jquery-placeholder/jquery.placeholder.js',
+
+*/
