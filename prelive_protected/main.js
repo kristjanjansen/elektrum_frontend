5051052c401289428fff2d8ffa20b8c9bf1b548d
kristjanjansen <kristjan.jansen@gmail.com>
2017-03-28 09:10:46 +0300

    latest

--- prelive_protected/main.js
+++ prelive_protected/main.js
@@ -8,10 +8,30 @@ import jQueryUiJs from 'jquery-ui-dist/jquery-ui.js'
 import jQueryUiTimepickerCss from 'jquery-ui-timepicker-addon/dist/jquery-ui-timepicker-addon.css'
 import jQueryUiTimepickerJs from 'jquery-ui-timepicker-addon/dist/jquery-ui-timepicker-addon.js'
 
-$('#datetimepicker').datetimepicker()
+import select2Css from 'select2/dist/css/select2.css'
+import select2Js from 'select2'
 
-$(function() {
+import bootstrap from 'bootstrap'
+import bootstrapCss from 'bootstrap/less/bootstrap.less'
 
-console.log($('#datetimepicker').datetimepicker())
+import bootstrapTokenfield from 'bootstrap-tokenfield'
+import bootstrapTokenfieldCss from 'bootstrap-tokenfield/dist/css/bootstrap-tokenfield.css'
+
+import bootbox from 'bootbox'
+
+import daterangepicker from 'daterangepicker'
+import daterangepickerCss from 'daterangepicker/daterangepicker-bs3.css'
+
+$(() => {
+
+    $('#datetimepicker').datetimepicker()
+    
+    $('#select2').select2()
+
+    // $('#tokenfield').tokenfield()
+
+    $('#bootbox').on('click', () => bootbox.alert("This is Bootbox"))
+
+    $('#daterange').daterangepicker()
 
 })
